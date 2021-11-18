56b8635f968bc46abba38dd67d16278da4c7a023
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-11-18 15:00:37 -0500

    The scaffolding exists. Now i right clicked on the project and then add new scaffolditem then identity and create .After that  i clicked on override all files and selected ApplicationDbContext (AuthenticatedWebApp.Data) in Data Context Class and add. After that i run Add-Migration "Initial Migration" and Update-Database.
    Everthing is working smoothly.

--- AuthenticatedWebApp/Data/Migrations/20211118195751_Initial Migration.Designer.cs
+++ AuthenticatedWebApp/Data/Migrations/20211118195751_Initial Migration.Designer.cs
@@ -4,14 +4,16 @@ using AuthenticatedWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AuthenticatedWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211118195751_Initial Migration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
