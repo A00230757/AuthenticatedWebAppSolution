9f0bfe7147ec0673f80f02295bafe1b11655b1e9
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-11-18 14:50:45 -0500

    The secret page now only be shown to Authenticated users.

--- AuthenticatedWebApp/Controllers/HomeController.cs
+++ AuthenticatedWebApp/Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace AuthenticatedWebApp.Controllers
             return View();
         }
 
-       
+        [Authorize]
         public IActionResult Secret()
         {
             return View();
